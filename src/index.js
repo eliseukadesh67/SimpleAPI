2bae346849b56a7198b4565562568948ce2fe2ec
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-10-15 17:35:13 -0300

    HOTFIX cors.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const routes = require('./routes.js');
 const app = express();
 const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors('localhost:3000'));
 
 app.listen(process.env.PORT || 3333);

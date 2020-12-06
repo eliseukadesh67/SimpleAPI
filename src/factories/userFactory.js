81fc18978ac53c3a164f032b88a17a2b8d9bafd1
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-12-05 22:11:27 -0300

    Adding an user factory file to generate an userFactory and an userService instances, and inject their given dependencies.

---
+++ src/factories/userFactory.js
@@ -0,0 +1,18 @@
+const UseRepository = require('../repositories/userRepository');
+const UserService = require('../services/userService');
+const { join } = require('path');
+const filename = join(__dirname, '../../database', 'data.json');
+const generateInstance = () =>{
+  const userRepository = new UseRepository({
+    file: filename
+  })
+  const userService = new UserService({
+    userRepository
+  })
+
+  return userService;
+}
+
+module.exports = { generateInstance };
+
+/* generateInstance().find().then(console.log); */

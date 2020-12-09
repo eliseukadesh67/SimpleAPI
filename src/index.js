6022b99a5f92660e895eeb01839d6342fc4ce328
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-12-08 23:38:25 -0300

    Adding /users:post route.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const PORT = 3000
 const DEFAULT_HEADER = {"Content-Type": "application/json"};
 const UserFactory = require('./factories/userFactory');
 const userService = UserFactory.generateInstance();
+const User = require('./entities/user');
 
 const routes = {
   '/users:get' : async(request, response) => {
@@ -11,9 +12,45 @@ const routes = {
     response.write(JSON.stringify({results: users}));
     return response.end();
   },
+  '/users:post': async (request, response) => {
+    // async iterator
+    for await (const data of request) {
+        try {
+            const item = JSON.parse(data);
+            const user = new User(item);
+            const { error, valid } = user.isValid();
+            if (!valid) {
+                response.writeHead(400, DEFAULT_HEADER)
+                response.write(JSON.stringify({ error: error.join(',') }))
+                return response.end()
+            }
+
+            const id = await userService.create(user)
+            response.writeHead(201, DEFAULT_HEADER)
+            response.write(JSON.stringify({ success: 'User Created with success!!', id }))
+
+            // só jogamos o return aqui pois sabemos que é um objeto body por requisicao
+            // se fosse um arquivo, que sobe sob demanda 
+            // ele poderia entrar mais vezes em um mesmo evento, aí removeriamos o return
+            return response.end()
+        } catch (error) {
+            return handleError(response)(error);
+        }
+    }
+  },
   default: (request, response) => {
     response.write('Hello!');
     response.end();
+  }
+}
+
+const handleError = response => {
+  return error => {
+      console.error('Deu Ruim!***', error)
+      response.writeHead(500, DEFAULT_HEADER)
+      response.write(JSON.stringify({ error: 'Internal Server Error!!' }))
+
+      return response.end();
   }
 }
 

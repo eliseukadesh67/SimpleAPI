408e0e2a08a2b01e4bf5ac5d203994ae9bcca774
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-03-24 16:04:13 -0300

    Fixing the indenting.

--- src/factories/userFactory.js
+++ src/factories/userFactory.js
@@ -2,7 +2,7 @@ const UseRepository = require('../repositories/userRepository');
 const UserService = require('../services/userService');
 const { join } = require('path');
 const filename = join(__dirname, '../../database', 'data.json');
-const generateInstance = () =>{
+const generateInstance = () => {
   const userRepository = new UseRepository({
     file: filename
   })
--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 const http = require('http');
 const PORT = 3000
-const DEFAULT_HEADER = {"Content-Type": "application/json"};
+const DEFAULT_HEADER = { "Content-Type": "application/json" };
 const UserFactory = require('./factories/userFactory');
 const userService = UserFactory.generateInstance();
 const User = require('./entities/user');
 
 const routes = {
-  '/users:get' : async(request, response) => {
+  '/users:get': async (request, response) => {
     const { id } = request.queryString;
     const users = await userService.find(id);
-    response.write(JSON.stringify({results: users}));
+    response.write(JSON.stringify({ results: users }));
     return response.end();
   },
   '/users:post': async (request, response) => {
     // async iterator
     for await (const data of request) {
-        try {
-            const item = JSON.parse(data);
-            const user = new User(item);
-            const { error, valid } = user.isValid();
-            if (!valid) {
-                response.writeHead(400, DEFAULT_HEADER)
-                response.write(JSON.stringify({ error: error.join(',') }))
-                return response.end()
-            }
-
-            const id = await userService.create(user)
-            response.writeHead(201, DEFAULT_HEADER)
-            response.write(JSON.stringify({ success: 'User Created with success!!', id }))
-
-            // só jogamos o return aqui pois sabemos que é um objeto body por requisicao
-            // se fosse um arquivo, que sobe sob demanda 
-            // ele poderia entrar mais vezes em um mesmo evento, aí removeriamos o return
-            return response.end()
-        } catch (error) {
-            return handleError(response)(error);
+      try {
+        const item = JSON.parse(data);
+        const user = new User(item);
+        const { error, valid } = user.isValid();
+        if (!valid) {
+          response.writeHead(400, DEFAULT_HEADER)
+          response.write(JSON.stringify({ error: error.join(',') }))
+          return response.end()
         }
+
+        const id = await userService.create(user)
+        response.writeHead(201, DEFAULT_HEADER)
+        response.write(JSON.stringify({ success: 'User Created with success!!', id }))
+
+        // só jogamos o return aqui pois sabemos que é um objeto body por requisicao
+        // se fosse um arquivo, que sobe sob demanda 
+        // ele poderia entrar mais vezes em um mesmo evento, aí removeriamos o return
+        return response.end()
+      } catch (error) {
+        return handleError(response)(error);
+      }
     }
   },
   default: (request, response) => {
@@ -46,18 +46,18 @@ const routes = {
 
 const handleError = response => {
   return error => {
-      console.error('Deu Ruim!***', error)
-      response.writeHead(500, DEFAULT_HEADER)
-      response.write(JSON.stringify({ error: 'Internal Server Error!!' }))
+    console.error('Deu Ruim!***', error)
+    response.writeHead(500, DEFAULT_HEADER)
+    response.write(JSON.stringify({ error: 'Internal Server Error!!' }))
 
-      return response.end();
+    return response.end();
   }
 }
 
 const handler = (request, response) => {
   const { url, method } = request
   const [first, route, id] = url.split('/');
-  request.queryString = {id: isNaN(id) ? id : Number(id)};
+  request.queryString = { id: isNaN(id) ? id : Number(id) };
 
   const key = `/${route}:${method.toLowerCase()}`;
 
